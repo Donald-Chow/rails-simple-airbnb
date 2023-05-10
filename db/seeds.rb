9f806a3bf076ec929e776c8be4c5eb0f51bc1d92
Donald Chow <donald7290@gmail.com>
2023-05-11 00:54:37 +0900

    Complete Rails Simple Airbnb Challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning the db....'
+Flats.destroy_all
+
+puts 'Creating flats..'
+
+10.times do
+  Flat.create!(
+    name: Faker::JapaneseMedia::SwordArtOnline.location,
+    address: Faker::Address.full_address,
+    description: Faker::JapaneseMedia::StudioGhibli.quote,
+    price_per_night: (rand(1..30) * 10),
+    number_of_guests: rand(2..10)
+  )
+end
+
+puts "... created #{Flats.count} houses"
